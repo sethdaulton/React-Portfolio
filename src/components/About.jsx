cfa64015c9133173633fefb0d3477d1e0ffa8943
Seth Daulton <seth.daulton@gmail.com>
2023-02-14 10:24:07 -0600

    Made updates to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,1 +1,23 @@
 import React from 'react'
+
+const About = () => {
+    return (
+    <div name='about' className='w-full h-screen bg-white text-black'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                <div className='test-4xl font-bold inline border-b-4 border-black'>About</div>
+            </div>
+        </div>
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 mx-[500px]'>
+            <div className='sm:text-right text-3xl font-bold'>
+                <p>Budding Web Developer and experienced Graphic Designer looking for a new challenge in tech</p>
+            </div>
+            <div className='text-lg'>
+                <p>I have an Master of Fine Arts degree in Printmaking which contributes to my ability to solve problems creatively and several years of experience in graphic design leading to a strong team member in front-end web development. I also posses a strong set of communication skills and interpersonal skills making me a strong member of a small or large team.</p>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default About
